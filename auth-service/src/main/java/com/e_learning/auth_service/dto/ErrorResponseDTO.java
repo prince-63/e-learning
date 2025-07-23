e2f0cd2447f63405d33e9a75accd2e521b3b0932
Prince Kumar Prasad <prince08833@gmail.com>
2025-07-23 13:50:45 +0530

    auth-service:feat: added a error handlers

---
+++ auth-service/src/main/java/com/e_learning/auth_service/dto/ErrorResponseDTO.java
@@ -0,0 +1,41 @@
+package com.e_learning.auth_service.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+@Schema(description = "Generic structure for API error responses")
+public class ErrorResponseDTO {
+
+    @Schema(
+            description = "The endpoint path where the error occurred",
+            example = "/api/events/123"
+    )
+    private String apiPath;
+
+    @Schema(
+            description = "HTTP status code associated with the error",
+            example = "404"
+    )
+    private int errorCode;
+
+    @Schema(
+            description = "Detailed error message",
+            example = "Event not found with the given ID"
+    )
+    private String errorMessage;
+
+    @Schema(
+            description = "The time when the error occurred",
+            example = "2025-07-15T14:30:00"
+    )
+    private LocalDateTime errorTime;
+}
