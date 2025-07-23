c38be46ebc473dde2d23df7112e362ddc4dc141c
Prince Kumar Prasad <prince08833@gmail.com>
2025-07-23 12:31:35 +0530

    auth-service: initial commit

---
+++ auth-service/src/test/java/com/e_learning/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_learning.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
