878fbb02a528d1866782be2a4678e6f83cca9604
Prince Kumar Prasad <prince08833@gmail.com>
2025-07-23 13:52:19 +0530

    auth-service:feat: add user repository

---
+++ auth-service/src/main/java/com/e_learning/auth_service/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.e_learning.auth_service.repositories;
+
+import com.e_learning.auth_service.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String username);
+}
