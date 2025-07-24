5c47f5f0820d28ff6c780bba61c5de31d5c90345
Prince Kumar Prasad <prince08833@gmail.com>
2025-07-24 16:04:02 +0530

    initial commit for presigned url generator service

---
+++ presigned-url-generator-service/src/main/java/com/e_learning/presigned_url_generator_service/PresignedUrlGeneratorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.e_learning.presigned_url_generator_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresignedUrlGeneratorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresignedUrlGeneratorServiceApplication.class, args);
+	}
+
+}
---
+++ presigned-url-generator-service/src/test/java/com/e_learning/presigned_url_generator_service/PresignedUrlGeneratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_learning.presigned_url_generator_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresignedUrlGeneratorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
