0a324b76e80ca701473b4d96a581faeb53e3593f
Prince Kumar Prasad <prince08833@gmail.com>
2025-07-28 16:10:20 +0530

    feat: add mappers

---
+++ user-service/src/main/java/com/e_learning/user_service/mappers/UserDetailsMapper.java
@@ -0,0 +1,23 @@
+package com.e_learning.user_service.mappers;
+
+import com.e_learning.user_service.dto.AuthResponseDTO;
+import com.e_learning.user_service.dto.UserDetailsResponseDTO;
+import com.e_learning.user_service.entities.UserDetails;
+
+public class UserDetailsMapper {
+
+    public static UserDetailsResponseDTO mapUserDetailsResponseDTO(UserDetails userDetails, AuthResponseDTO authResponseDTO) {
+        return UserDetailsResponseDTO.builder()
+                .detailsId(userDetails.getDetailsId())
+                .userId(userDetails.getUserId())
+                .fullName(authResponseDTO.getFullName())
+                .email(authResponseDTO.getEmail())
+                .profession(userDetails.getProfession())
+                .bio(userDetails.getBio())
+                .avatarUrl(userDetails.getAvatarUrl())
+                .phoneNumber(userDetails.getPhoneNumber())
+                .roles(authResponseDTO.getRole())
+                .active(authResponseDTO.isActive())
+                .build();
+    }
+}
