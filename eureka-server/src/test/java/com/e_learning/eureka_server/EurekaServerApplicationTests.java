1316831a0bbce9b1a049ac9008dd8eadbef79a99
Prince Kumar Prasad <prince08833@gmail.com>
2025-07-28 11:29:49 +0530

    feat: configured eureka server as service discovering.

---
+++ eureka-server/src/test/java/com/e_learning/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_learning.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
