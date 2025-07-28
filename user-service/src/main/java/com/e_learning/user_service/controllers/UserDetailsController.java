c3b73109a5c1edb13b9d8ec7e34b317c1e9ed437
Prince Kumar Prasad <prince08833@gmail.com>
2025-07-28 18:58:40 +0530

    doc: add doc for user details controller

--- user-service/src/main/java/com/e_learning/user_service/controllers/UserDetailsController.java
+++ user-service/src/main/java/com/e_learning/user_service/controllers/UserDetailsController.java
@@ -2,11 +2,14 @@ package com.e_learning.user_service.controllers;
 
 import com.e_learning.user_service.dto.*;
 import com.e_learning.user_service.services.UserDetailsService;
-import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,6 +23,13 @@ public class UserDetailsController {
 
     private final UserDetailsService userDetailsService;
 
+    @Operation(method = "GET",summary = "Get User Details")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successfully retrieved",
+                    content = @Content(schema = @Schema(implementation = ResponseDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid detailId"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     @GetMapping(GET_USER_DETAILS)
     public ResponseEntity<ResponseDTO<UserDetailsResponseDTO>> getUserDetails(
             @PathVariable String detailsId
@@ -28,35 +38,63 @@ public class UserDetailsController {
         return ResponseEntity.ok(new ResponseDTO<>("Successfully retrieved user details", true, userDetailsResponseDTO));
     }
 
-    @PatchMapping(UPDATE_BIO)
+    @Operation(method = "PATCH/POST",summary = "Update and add bio")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Request proceeded successfully",
+                    content = @Content(schema = @Schema(implementation = ResponseDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid detailId"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    @RequestMapping(value= UPDATE_BIO, method = {RequestMethod.POST, RequestMethod.PATCH})
     public ResponseEntity<ResponseDTO<UserDetailsResponseDTO>> updateBio(
             @PathVariable Long userId,
             @Valid @RequestBody BioRequestDTO dto) {
         UserDetailsResponseDTO updated = userDetailsService.updateBio(userId, dto);
-        return ResponseEntity.ok(new ResponseDTO<>("Successfully updated bio", true, updated));
+        return ResponseEntity.ok(new ResponseDTO<>("Request proceeded successfully", true, updated));
     }
 
-    @PatchMapping(value=UPDATE_AVATAR, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(method = "PATCH/POST",summary = "Update and add avatar")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Request proceeded successfully",
+                    content = @Content(schema = @Schema(implementation = ResponseDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid detailId"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    @RequestMapping(value= UPDATE_AVATAR, method = {RequestMethod.POST, RequestMethod.PATCH})
     public ResponseEntity<ResponseDTO<UserDetailsResponseDTO>> updateAvatar(
             @PathVariable Long userId,
             @RequestPart(name = "file") MultipartFile file) {
         UserDetailsResponseDTO updated = userDetailsService.updateAvatarUrl(userId, file);
-        return ResponseEntity.ok(new ResponseDTO<>("Successfully updated avatar", true, updated));
+        return ResponseEntity.ok(new ResponseDTO<>("Request proceeded successfully", true, updated));
     }
 
-    @PatchMapping(UPDATE_PROFESSION)
+    @Operation(method = "PATCH/POST",summary = "Update and add profession")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Request proceeded successfully",
+                    content = @Content(schema = @Schema(implementation = ResponseDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid detailId"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    @RequestMapping(value= UPDATE_PROFESSION, method = {RequestMethod.POST, RequestMethod.PATCH})
     public ResponseEntity<ResponseDTO<UserDetailsResponseDTO>> updateProfession(
-            @Parameter(description = "User ID") @PathVariable Long userId,
+            @PathVariable Long userId,
             @Valid @RequestBody ProfessionRequestDTO dto) {
         UserDetailsResponseDTO updated = userDetailsService.updateProfession(userId, dto);
-        return ResponseEntity.ok(new ResponseDTO<>("Successfully updated profession", true, updated));
+        return ResponseEntity.ok(new ResponseDTO<>("Request proceeded successfully", true, updated));
     }
 
-    @PatchMapping(UPDATE_PHONE)
+    @Operation(method = "PATCH/POST",summary = "Update and add phone number")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Request proceeded successfully",
+                    content = @Content(schema = @Schema(implementation = ResponseDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid detailId"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    @RequestMapping(value= UPDATE_PHONE, method = {RequestMethod.POST, RequestMethod.PATCH})
     public ResponseEntity<ResponseDTO<UserDetailsResponseDTO>> updatePhoneNumber(
-            @Parameter(description = "User ID") @PathVariable Long userId,
+            @PathVariable Long userId,
             @Valid @RequestBody PhoneNumberRequestDTO dto) {
         UserDetailsResponseDTO updated = userDetailsService.updatePhoneNumber(userId, dto);
-        return ResponseEntity.ok(new ResponseDTO<>("Successfully updated phone number", true, updated));
+        return ResponseEntity.ok(new ResponseDTO<>("Request proceeded successfully", true, updated));
     }
 }
