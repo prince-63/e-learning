0e022c7c04bc4bb36730804e2d5b027c8a20f194
Prince Kumar Prasad <prince08833@gmail.com>
2025-07-29 09:39:49 +0530

    feat: implemented AOP for logging in presigned url generator service

---
+++ presigned-url-generator-service/src/main/java/com/e_learning/presigned_url_generator_service/aspects/LoggerAspects.java
@@ -0,0 +1,35 @@
+package com.e_learning.presigned_url_generator_service.aspects;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.Instant;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggerAspects {
+
+    @Around("execution(* com.e_learning.presigned_url_generator_service..*.*(..))")
+    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("{} method execution start", joinPoint.getSignature().toString());
+        Instant start = Instant.now();
+        Object returnObj = joinPoint.proceed();
+        Instant end = Instant.now();
+        long timeElapsed = Duration.between(start, end).toMillis();
+        log.info("Time took to execute {} method is : {}", joinPoint.getSignature().toString(), timeElapsed);
+        log.info("{} method execution end", joinPoint.getSignature().toString());
+        return returnObj;
+    }
+
+    @AfterThrowing(value = "execution(* com.e_learning.presigned_url_generator_service.*.*(..))", throwing = "ex")
+    public void logException(JoinPoint joinPoint, Exception ex) {
+        log.error("{} An exception happened due to : {}", joinPoint.getSignature(), ex.getMessage());
+    }
+}
