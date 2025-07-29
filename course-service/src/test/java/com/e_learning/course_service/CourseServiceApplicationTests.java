d41cf60f7966ad8344b9ee8ad360b0ea36dc4b4e
Prince Kumar Prasad <prince08833@gmail.com>
2025-07-29 09:51:42 +0530

    init course service

---
+++ course-service/src/test/java/com/e_learning/course_service/CourseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_learning.course_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
