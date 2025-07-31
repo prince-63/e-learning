4afb7d1bbd028a83d1871f70052756eb3b5784e1
Prince Kumar Prasad <prince08833@gmail.com>
2025-07-31 11:48:35 +0530

    feat: add course controller and expose create course api endpoint

---
+++ course-service/src/main/java/com/e_learning/course_service/controllers/CourseController.java
@@ -0,0 +1,35 @@
+package com.e_learning.course_service.controllers;
+
+import com.e_learning.course_service.collections.Course;
+import com.e_learning.course_service.dto.CourseRequestDTO;
+import com.e_learning.course_service.dto.CourseResponseDTO;
+import com.e_learning.course_service.dto.ResponseDTO;
+import com.e_learning.course_service.mappers.CourseMapper;
+import com.e_learning.course_service.services.CourseService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.e_learning.course_service.constants.ApiPathConstants.CREATE_COURSE;
+
+@RestController
+@AllArgsConstructor
+public class CourseController {
+
+    private final CourseService courseService;
+
+    @PostMapping(CREATE_COURSE)
+    public ResponseEntity<ResponseDTO<CourseResponseDTO>> createCourse(@RequestBody @Valid CourseRequestDTO courseRequestDTO) {
+        Course course = courseService.createCourse(courseRequestDTO);
+        ResponseDTO<CourseResponseDTO> response = new ResponseDTO<>();
+        response.setSuccess(true);
+        response.setMessage("Course created successfully");
+        response.setData(CourseMapper.CourseToDto(course));
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+}
