5bc5e4b28349bd1db9876976f3b4f7ac8741de40
Prince Kumar Prasad <prince08833@gmail.com>
2025-08-04 12:42:35 +0530

    config: configure api formate and constants for sections

---
+++ course-service/src/main/java/com/e_learning/course_service/constants/SectionApiConstants.java
@@ -0,0 +1,8 @@
+package com.e_learning.course_service.constants;
+
+import static com.e_learning.course_service.constants.BaseApiPathConstants.BASE;
+
+public final class SectionApiConstants {
+    private static final String SECTION_BASE = BASE + "section/";
+    public static final String ADD_SECTION = SECTION_BASE + "create";
+}
