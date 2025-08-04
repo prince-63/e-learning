77d7e572bde325c416e749a60de5f855cf3bcb76
Prince Kumar Prasad <prince08833@gmail.com>
2025-08-04 13:12:02 +0530

    refactor: change api signature

--- course-service/src/main/java/com/e_learning/course_service/constants/CourseApiConstants.java
+++ course-service/src/main/java/com/e_learning/course_service/constants/CourseApiConstants.java
@@ -4,7 +4,7 @@ import static com.e_learning.course_service.constants.BaseApiPathConstants.BASE;
 
 public final class CourseApiConstants {
     private static final String COURSE_BASE = BASE + "course/";
-    public static final String CREATE_COURSE = COURSE_BASE + "create";
+    public static final String CREATE_COURSE = COURSE_BASE + "add";
     public static final String ADD_THUMBNAIL = COURSE_BASE + "add-thumbnail";
-    public static final String UPDATE_COURSE_DETAILS = COURSE_BASE + "update-details";
+    public static final String UPDATE_COURSE_DETAILS = COURSE_BASE + "update";
 }
--- course-service/src/main/java/com/e_learning/course_service/constants/SectionApiConstants.java
+++ course-service/src/main/java/com/e_learning/course_service/constants/SectionApiConstants.java
@@ -4,6 +4,6 @@ import static com.e_learning.course_service.constants.BaseApiPathConstants.BASE;
 
 public final class SectionApiConstants {
     private static final String SECTION_BASE = BASE + "section/";
-    public static final String ADD_SECTION = SECTION_BASE + "create";
+    public static final String ADD_SECTION = SECTION_BASE + "add";
     public static final String UPDATE_SECTION = SECTION_BASE + "update";
 }
