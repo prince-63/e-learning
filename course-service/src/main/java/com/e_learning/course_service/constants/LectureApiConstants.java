278b327ccd99ca75d23e2d304f2512156020ccf6
Prince Kumar Prasad <prince08833@gmail.com>
2025-08-04 13:18:42 +0530

    feat: add lecture api constants

---
+++ course-service/src/main/java/com/e_learning/course_service/constants/LectureApiConstants.java
@@ -0,0 +1,8 @@
+package com.e_learning.course_service.constants;
+
+import static com.e_learning.course_service.constants.BaseApiPathConstants.BASE;
+
+public final class LectureApiConstants {
+    private static final String LECTURE_BASE = BASE + "lecture/";
+    public static final String ADD_LECTURE = LECTURE_BASE + "add";
+}
