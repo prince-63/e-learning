fb9bc8e614eff353f96eccec683d7298e9b60c95
Prince Kumar Prasad <prince08833@gmail.com>
2025-08-04 13:18:00 +0530

    feat: implement mapper of lecture dto's

---
+++ course-service/src/main/java/com/e_learning/course_service/mappers/LectureMapper.java
@@ -0,0 +1,31 @@
+package com.e_learning.course_service.mappers;
+
+import com.e_learning.course_service.collections.Lecture;
+import com.e_learning.course_service.dto.LectureRequestDTO;
+import com.e_learning.course_service.dto.LectureResponseDTO;
+
+public class LectureMapper {
+
+    public static Lecture toLecture(LectureRequestDTO lectureRequestDTO) {
+        return Lecture.builder()
+                .title(lectureRequestDTO.getTitle())
+                .order(lectureRequestDTO.getOrder())
+                .duration(lectureRequestDTO.getDuration())
+                .build();
+    }
+
+    public static LectureResponseDTO toDTO(Lecture lecture) {
+        return LectureResponseDTO.builder()
+                .lectureId(lecture.getLectureId())
+                .sectionId(lecture.getSectionId())
+                .title(lecture.getTitle())
+                .order(lecture.getOrder())
+                .duration(lecture.getDuration())
+                .videoUrlPublicId(lecture.getVideoUrlPublicId())
+                .videoUrl(lecture.getVideoUrl())
+                .preview(lecture.isPreview())
+                .resourceUrls(lecture.getResourceUrls())
+                .updatedAt(lecture.getUpdatedAt())
+                .build();
+    }
+}
