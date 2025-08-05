ee98d3e9337ac111a980fd46824379982dfd3109
Prince Kumar Prasad <prince08833@gmail.com>
2025-08-05 19:44:09 +0530

    refactor: change the api signature

--- presigned-url-generator-service/src/main/java/com/e_learning/presigned_url_generator_service/constants/ApiConstants.java
+++ presigned-url-generator-service/src/main/java/com/e_learning/presigned_url_generator_service/constants/ApiConstants.java
@@ -3,8 +3,8 @@ package com.e_learning.presigned_url_generator_service.constants;
 public final class ApiConstants {
     public static final String BASE = "/api/v1/media";
     public static final String WELCOME = BASE + "/welcome";
-    public static final String COURSE_PDF = BASE + "/section/pdf";
-    public static final String COURSE_VIDEO = BASE + "/course/video";
-    public static final String COURSE_IMAGE =  BASE + "/course/image";
+    public static final String LECTURE_RESOURCE = BASE + "/course/section/lecture/resource";
+    public static final String LECTURE_VIDEO = BASE + "/course/section/lecture/video";
+    public static final String BANNER_IMAGE =  BASE + "/course/banner/image";
     public static final String USER_PROFILE = BASE + "/user/profile-image";
 }
