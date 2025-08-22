f09c09ded97f19b7205d7bef803cd5c1d49438b0
Prince Kumar Prasad <prince08833@gmail.com>
2025-08-22 13:55:39 +0530

    refactor: unused imports removed

--- course-service/src/main/java/com/e_learning/course_service/collections/Resource.java
+++ course-service/src/main/java/com/e_learning/course_service/collections/Resource.java
@@ -1,9 +1,11 @@
 package com.e_learning.course_service.collections;
 
-import lombok.*;
 import io.swagger.v3.oas.annotations.media.Schema;
-
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
--- course-service/src/main/java/com/e_learning/course_service/dto/CourseRequestDTO.java
+++ course-service/src/main/java/com/e_learning/course_service/dto/CourseRequestDTO.java
@@ -1,7 +1,10 @@
 package com.e_learning.course_service.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
