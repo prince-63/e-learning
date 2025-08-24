4cb3271603133289aa9c6f8de57e591136007076
Prince Kumar Prasad <prince08833@gmail.com>
2025-08-24 10:00:46 +0530

    initial commit; notification service

---
+++ notification-service/src/test/java/com/e_learning/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_learning.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
